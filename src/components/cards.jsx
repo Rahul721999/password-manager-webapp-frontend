e23c1e091c46511637d30f8e48d88d2db9489fc1
Rahul721999 <rrmondal12@gmail.com>
2023-06-06 02:14:34 +0530

    Content loading on Dashboard now

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './card'
-const List = () => {
+import config from './config';
+const List = (props) => {
+    const {AuthToken} = props;
+    const [vec, setResVec] = useState([]);
+    useEffect(()=>{
+        callGetAll(AuthToken, setResVec);
+    },[AuthToken]);
+
+    // handle Delete Card Request
+    const handleDeleteCard= (cardId)=>{
+        setResVec(prevVec => prevVec.filter(card => card.id !== cardId));
+    };
+
     return (
         <div className='card-list'>
-            <Card/>
-            <Card/>
-            <Card/>
+            {vec.map((element)=>{
+                return (
+                    <Card
+                        key={element.id}
+                        cardId={element.id}
+                        cardName={element.website_name}
+                        url={element.website_url} 
+                        username={element.username} 
+                        password={element.password}
+                        token={AuthToken} // pass the token to props
+                        onDelete={handleDeleteCard} // pass the callback function to props 
+                    />)
+            })}
         </div>
     )
 }
 
 export default List;
+
+async function callGetAll(AuthToken, setResVec){
+    const base_url = config.myEnvVar;
+    let result;
+    try {
+        const response = await fetch(base_url.concat("/User/get_all"), {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "/",
+                "Connection": "keep-alive",
+                "Authorization" : AuthToken,
+            },
+        })
+        if (response.ok) {
+            const data = await response.json();
+            result = data.data;
+            setResVec(result);
+        } else {
+            const data = await response.json();
+            const message = data.message;
+            alert(message);
+        }
+    } catch (err) {
+        console.log("An Error Occured:", err);
+    }
+}
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import List from "./cards";
-
+// import config from "./config";
+import { useLocation,useNavigate } from "react-router-dom";
 const Dashboard = () => {
+  const navigate=useNavigate();
+  const location = useLocation();
+  const {token} = location.state || {};
+  useEffect(()=>{
+    if (!token){
+      return(navigate('/'))
+    }
+  },[navigate,token])
+  
   return (
     <div className="dashboard">
       <div className="heading-section">
-        <h2>Welcome</h2>
+        <h2 >Welcome</h2>
       </div>
       <div className="content-section">
-        <List/>
+        <List AuthToken={token}/>
+      </div>
+      <div className="dashboard-footer">
+        FOOTER
       </div>
     </div>
   );
